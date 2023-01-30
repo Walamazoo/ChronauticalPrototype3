14a18264162310a5cc997cf68b8b8a1e52b20c27
Walamazoo <59935897+Walamazoo@users.noreply.github.com>
2023-01-30 00:21:38 -0500

    Clicking NPCs starts convos

--- Chronautical Prototype 3/Assets/Scenes/Prototype/SpriteInteraction.cs
+++ Chronautical Prototype 3/Assets/Scenes/Prototype/SpriteInteraction.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Ink.Runtime;
+using Ink.UnityIntegration;
+using PixelCrushers.DialogueSystem.InkSupport;
+using PixelCrushers.DialogueSystem;
 
 public class SpriteInteraction : MonoBehaviour
 {
+    [SerializeField] DialogueSystemInkTrigger trigger;
     private bool interactable= false;
 
     public void ToggleInteraction(bool state){
@@ -12,7 +17,7 @@ public class SpriteInteraction : MonoBehaviour
 
     void OnMouseDown(){
         if(interactable){
-            Debug.Log(this.name + "was clicked");
+            trigger.OnUse();
         }
     }
 }
