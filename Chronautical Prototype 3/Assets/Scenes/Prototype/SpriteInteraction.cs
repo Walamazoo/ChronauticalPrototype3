f243ad09d0a58b3d8fb8c037eb0ce1927ef4d471
Walamazoo <59935897+Walamazoo@users.noreply.github.com>
2023-02-05 01:19:49 -0500

    MacOS Build

--- Chronautical Prototype 3/Assets/Scenes/Prototype/SpriteInteraction.cs
+++ Chronautical Prototype 3/Assets/Scenes/Prototype/SpriteInteraction.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Ink.Runtime;
-using Ink.UnityIntegration;
+//using Ink.UnityIntegration;
 using PixelCrushers.DialogueSystem.InkSupport;
 using PixelCrushers.DialogueSystem;
 
