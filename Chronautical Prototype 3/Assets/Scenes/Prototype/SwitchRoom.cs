6a39a2774c8200bad4d3c4b54f4084adcad79cc9
AidanMolina <63667680+AidanMolina@users.noreply.github.com>
2023-02-07 22:47:15 -0500

    Ink script traversal fixing

--- Chronautical Prototype 3/Assets/Scenes/Prototype/SwitchRoom.cs
+++ Chronautical Prototype 3/Assets/Scenes/Prototype/SwitchRoom.cs
@@ -9,6 +9,8 @@ public class SwitchRoom : MonoBehaviour
     //Get all of the object references that are needed to make each button move and change things properly
     [SerializeField] GameObject MiniCharacter; //The mini character on the minimap to show where the player is
     [SerializeField] string roomName; //Name of where the player currently is
+    [SerializeField] GameObject myButton; //The button on the Time Slider that is used to start an Ink script. So Library Button gets LibraryTimeButton.
+    [SerializeField] GameObject[] buttons; //All of the buttons on the Time Slider LibraryTimeButton, LabTimeButton, MarketplaceTimeButton
     [SerializeField] SliderController controller; //The SliderController script from the Time Slider object
     [SerializeField] GameObject text; //The textbox that roomName is put into.
     [SerializeField] EnvironmentFilter environmentFilter;
@@ -18,6 +20,17 @@ public class SwitchRoom : MonoBehaviour
         //This way when we time travel, the script will always be on the ink script we want.
         //Also when we travel by the minimap, we run the TimeButton's ink script automatically. This is handled in the Unity Inspector's On Click.
         controller.currentLocation = roomName;
+        foreach(GameObject button in buttons)
+        {
+            if(button == myButton)
+            {
+                button.SetActive(true);
+            }
+            else
+            {
+                button.SetActive(false);
+            }
+        }
         
         StartCoroutine(MoveMiniCharacter()); //Moves the mini character to this button's position
         switch(roomName){ //Sets the text box's text to our roomName
