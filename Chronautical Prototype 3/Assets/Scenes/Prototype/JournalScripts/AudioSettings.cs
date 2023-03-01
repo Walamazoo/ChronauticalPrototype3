1590c973b2e300c294e467d1fd2ddda54c1bb92b
AidanMolina <63667680+AidanMolina@users.noreply.github.com>
2023-03-01 15:18:47 -0500

    Journal Audio Sliders added

---
+++ Chronautical Prototype 3/Assets/Scenes/Prototype/JournalScripts/AudioSettings.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioSettings : MonoBehaviour
+{
+    [SerializeField] Slider Master;
+    [SerializeField] Slider Music;
+    [SerializeField] Slider Ambience;
+    [SerializeField] Slider SoundEffects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Music.value = 0.5f;
+        Ambience.value = 0.5f;
+        SoundEffects.value = 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MusicVolumeLevel(){
+        FMODUnity.RuntimeManager.StudioSystem.setParameterByName("MusicVolume", Music.value);
+        Debug.Log(Music.value);
+    }
+
+    public void SoundEffectsVolumeLevel(){
+        FMODUnity.RuntimeManager.StudioSystem.setParameterByName("SFXVolume", SoundEffects.value);
+        Debug.Log(SoundEffects.value);
+    }
+
+    public void AmbienceVolumeLevel(){
+        FMODUnity.RuntimeManager.StudioSystem.setParameterByName("AmbienceVolume", Ambience.value);
+        Debug.Log(Ambience.value);
+    }
+}
